c3ac2b492a41c90816050cee3b4e73bb59e1109d
Harsh Thakkar <harshthakkar46@gmail.com>
2016-04-23 12:51:14 +0530

    status bar and navigation bar colour changed

--- TraveLibro/ViewController.swift
+++ TraveLibro/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mainBlueColor = UIColor(red: 0.1725, green: 0.2175, blue: 0.3412, alpha: 1)
+    let avenirFont = UIFont(name: "Avenir", size: 14)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // adding status bar view
+        let view = UIView(frame: CGRect(x: 0.0, y: 0.0, width: UIScreen.mainScreen().bounds.size.width, height: 20.0))
+        view.backgroundColor = mainBlueColor
+        self.view.addSubview(view)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
