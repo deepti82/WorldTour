271106c4d672a5af0e514fd5056d831d5bd83364
Harsh Thakkar <harshthakkar46@gmail.com>
2016-04-25 16:59:59 +0530

    many things added

--- TraveLibro/ViewController.swift
+++ TraveLibro/ViewController.swift
@@ -25,13 +25,29 @@ class ViewController: UIViewController {
         //UILabel.appearance().font = avenirFont
         button.titleLabel!.font = avenirFont
         
-        let journey = JourneyTitleView(frame: CGRectMake(0, 0, self.view.frame.size.width, 70))
-        journey.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
-        self.view.addSubview(journey)
-        
-        let circle = AddCircle(frame: CGRectMake(0, 0, 50, 50))
-        circle.center = CGPointMake(self.view.frame.size.width - 40, self.view.frame.size.height - 100)
-        self.view.addSubview(circle)
+//        let journey = JourneyTitleView(frame: CGRectMake(0, 0, self.view.frame.size.width, 65))
+//        journey.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2 - 75)
+//        self.view.addSubview(journey)
+//        
+//        let journeyImageView = UIView(frame: CGRectMake(0, 0, self.view.frame.size.width, 180))
+//        journeyImageView.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2 + 125 - 80)
+//        let image = UIImage(named: "london_image")
+//        let journeyImage = UIImageView(frame: CGRectMake(0, 0, journeyImageView.frame.size.width, journeyImageView.frame.size.height))
+//        journeyImage.image = image!
+//        //journeyImage.center = CGPointMake(journeyImageView.frame.size.width, 100)
+//        journeyImageView.addSubview(journeyImage)
+//        journeyImageView.layer.shadowOffset = CGSize(width: 2, height: 2)
+//        journeyImageView.layer.shadowOpacity = 0.2
+//        journeyImageView.layer.shadowRadius = 1
+//        self.view.addSubview(journeyImageView)
+//        
+//        let addcircle = AddCircle(frame: CGRectMake(0, 0, 50, 50))
+//        addcircle.center = CGPointMake(self.view.frame.size.width - 40, self.view.frame.size.height - 100)
+//        self.view.addSubview(addcircle)
+//        
+//        let infocircle = InfoCircle(frame: CGRectMake(0, 0, 40, 40))
+//        infocircle.center = CGPointMake(40, self.view.frame.size.height - 95)
+//        self.view.addSubview(infocircle)
         
     }
 
