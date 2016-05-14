cdce9579a880bcc0537bcb1bc1bb25ae8e861b49
Harsh Thakkar <harshthakkar46@gmail.com>
2016-05-14 12:19:27 +0530

    rating swift :sweat:

---
+++ TraveLibro/Rating.swift
@@ -0,0 +1,39 @@
+//
+//  Rating.swift
+//  TraveLibro
+//
+//  Created by Harsh Thakkar on 14/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class Rating: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "Rating", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(view);
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
