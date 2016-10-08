097bad85f289619816d8f9bfdd89c3db3fde5fe8
Harsh Thakkar <harshthakkar46@gmail.com>
2016-10-08 19:33:37 +0530

    rating started

---
+++ TraveLibro/RatingCheckIn.swift
@@ -0,0 +1,31 @@
+//
+//  RatingCheckIn.swift
+//  TraveLibro
+//
+//  Created by Harsh Thakkar on 08/10/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class RatingCheckIn: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "RatingCheckIn", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(view)
+    }
+    
+}
