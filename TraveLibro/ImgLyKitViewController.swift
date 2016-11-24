13548ef5e89bf183d0dd93b87a1a88598fa70da2
jagruti <jagruti@wohlig.com>
2016-11-24 12:46:42 +0530

    Revert "git"
    This reverts commit 850ba3f956f89be53fa7b4fea6970da3c74ef60d.

--- TraveLibro/ImgLyKitViewController.swift
+++ TraveLibro/ImgLyKitViewController.swift
@@ -13,62 +13,17 @@ class ImgLyKitViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let configuration = Configuration() { builder in
-//            builder.backgroundColor = UIColor.clear
-//        }
-//        let cameraViewController = CameraViewController(configuration:configuration)
-//        present(cameraViewController, animated: true, completion: nil)
-        
-        let cameraViewController = IMGLYCameraViewController(recordingModes: [.Photo, .Video])
-        cameraViewController.maximumVideoLength = 15
-        cameraViewController.squareMode = true
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = cameraViewController
-        window?.makeKeyAndVisible()
+        let cameraViewController = CameraViewController()
+        present(cameraViewController, animated: true, completion: nil)
+        // Do any additional setup after loading the view.
+    }
 
-    }
-    
-    private func customizeCameraController(builder: ConfigurationBuilder) {
-        builder.configureCameraViewController { options in
-            // Enable/Disable some features
-            options.maximumVideoLength = 15
-            options.showFilterIntensitySlider = false
-            options.tapToFocusEnabled = false
-            
-            // Use closures to customize the different view elements
-//            options.videoRecordingStartedHandler = { rec in
-//                print("clicked")
-//                print(rec)
-//            }
-            options.cameraRollButtonConfigurationClosure = { button in
-                button.layer.borderWidth = 2.0
-                button.layer.borderColor =  UIColor.red.cgColor
-            }
-            
-            options.timeLabelConfigurationClosure = { label in
-                label.textColor = UIColor.red
-            }
-            
-            options.recordingModeButtonConfigurationClosure = { button, _ in
-                button.setTitleColor(UIColor.gray, for: .normal)
-                button.setTitleColor(UIColor.red, for: .selected)
-            }
-            
-            // Force a selfie camera
-            options.allowedCameraPositions = [ .front ]
-            
-            // Disable flash
-            options.allowedFlashModes = [ .off ]
-        }
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+
     /*
     // MARK: - Navigation
 
@@ -79,17 +34,4 @@ class ImgLyKitViewController: UIViewController {
     }
     */
 
-}
-public final class VideoRecordButton: UIControl{
-    public var isRecording = true
-    public override func cancelTracking(with event: UIEvent?) {
-        print("canceled")
-    }
-    public override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-        print("recording started")
-        return true
-    }
-    public override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
-        print("recording end")
-    }
 }
