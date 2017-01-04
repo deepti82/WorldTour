153da3fb2f390d323b38a16ec980c296764d73a2
Pranay Joshi <pranayj1449517@gmail.com>
2017-01-04 18:27:22 +0530

    uploadCloudView

---
+++ TraveLibro/UploadingToCloud.swift
@@ -0,0 +1,36 @@
+//
+//  UploadingToCloud.swift
+//  TraveLibro
+//
+//  Created by Pranay Joshi on 04/01/17.
+//  Copyright © 2017 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class UploadingToCloud: UIView {
+
+    @IBOutlet weak var UploadingToCloudLabel: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    func loadViewFromNib() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "UploadingToCloud", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(view)
+        
+        
+          }
+    
+
+}
