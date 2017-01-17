00629fe3248f88d2bc23708f4ed4cc0b4b39ba6c
jagruti <jagruti@wohlig.com>
2017-01-17 19:21:23 +0530

    header

---
+++ TraveLibro/ActivityProfileHeader.swift
@@ -0,0 +1,34 @@
+//
+//  ActivityProfileHeader.swift
+//  TraveLibro
+//
+//  Created by Jagruti  on 17/01/17.
+//  Copyright © 2017 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class ActivityProfileHeader: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+        
+    }
+        required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func loadViewFromNib() {
+        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "ActivityProfileHeader", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(view)
+        
+    }
+
+
+}
