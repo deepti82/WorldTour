d426927ade7be6df44487a830b1b6a5f9f6c0d65
Chintan <chintan@wohlig.com>
2017-01-29 01:50:10 +0530

    yes removed kachara

--- Pods/Target Support Files/PullToMakeFlight/PullToMakeFlight-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PullToMakeFlight : NSObject
-@end
-@implementation PodsDummy_PullToMakeFlight
-@end
--- Pods/Target Support Files/PullToMakeFlight/PullToMakeFlight-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "PullToMakeFlight.h"
-
-FOUNDATION_EXPORT double PullToMakeFlightVersionNumber;
-FOUNDATION_EXPORT const unsigned char PullToMakeFlightVersionString[];
-
