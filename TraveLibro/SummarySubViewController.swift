d527bda17f4b4f9d47a8ad5b576e5813a19e0a3f
jagruti <jagruti@wohlig.com>
2017-01-29 16:00:52 +0530

    gito

--- TraveLibro/SummarySubViewController.swift
+++ TraveLibro/SummarySubViewController.swift
@@ -290,6 +290,15 @@ class SummarySubViewController: UIViewController, UIScrollViewDelegate {
         //  TOP VIEW
         let tripView = TripSummaryView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 144))
         tripView.allData = tripCountData
+        tripView.likes.text = tripCountData["likeCount"].stringValue
+        
+        let dateFormatter = DateFormatter()
+        let timeFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSZ"
+        let date = dateFormatter.date(from: tripCountData["startTime"].string!)
+        dateFormatter.dateFormat = "dd MMM, yyyy"
+        timeFormatter.dateFormat = "hh:mm a"
+        tripView.tripDate.text = "\(dateFormatter.string(from: date!))"
         
         tripView.profilePic.hnk_setImageFromURL(URL(string:"\(adminUrl)upload/readFile?file=\(tripCountData["user"]["profilePicture"].stringValue)")!)
         tripView.profileName.text! = tripCountData["user"]["name"].stringValue
