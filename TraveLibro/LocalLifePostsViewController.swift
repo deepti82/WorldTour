5f28248eacfb0b18e866a8d48ffbca2d4bc90271
Pranay Joshi <pranayj1449517@gmail.com>
2017-02-07 15:53:01 +0530

    git

--- TraveLibro/LocalLifePostsViewController.swift
+++ TraveLibro/LocalLifePostsViewController.swift
@@ -231,6 +231,7 @@ class LocalLifePostsViewController: UIViewController, UIScrollViewDelegate, CLLo
             let nearMeListController = storyboard?.instantiateViewController(withIdentifier: "nearMeListVC") as! NearMeListViewController
     nearMeListController.nearMeType = self.nearMeType
     self.navigationController?.pushViewController(nearMeListController, animated: true)
+        
        }
     
     
