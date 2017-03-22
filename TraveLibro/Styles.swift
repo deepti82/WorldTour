8b5d302cc4a9b6cf91f78981c2bc93817f99ec36
Supriya Malgaonkar <supriya.malgaonkar@wohlig.com>
2017-03-22 13:42:01 +0530

    Loader

--- TraveLibro/Styles.swift
+++ TraveLibro/Styles.swift
@@ -198,14 +198,17 @@ open class LoadingOverlay{
     func showOverlay(_ view: UIView) {
         hideOverlayView()
         print("show loader")
-        loader = UIView(frame:CGRect(x: 0, y: 0, width: 100, height: 100))
-        view.addSubview(loader)
-        loader.center = CGPoint(x: view.center.x, y: ((view.frame.size.height/2) - ( globalNavigationController != nil ? (globalNavigationController?.navigationBar.frame.size.height)! : 0) ))
+        loader = UIView(frame:CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
         let imageView1 = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         imageView1.backgroundColor = UIColor.clear
         imageView1.image = UIImage.gif(name: "100_100")
         imageView1.contentMode = .scaleAspectFit
+        imageView1.center = CGPoint(x: view.center.x, y: ((view.frame.size.height/2) - ( globalNavigationController != nil ? (globalNavigationController?.navigationBar.frame.size.height)! : 0) ))
         loader.addSubview(imageView1)
+        
+        loader.backgroundColor = UIColor(colorLiteralRed: 0/255, green: 0/255, blue: 0/255, alpha: 0.4)
+        view.addSubview(loader)
         
     }
     
