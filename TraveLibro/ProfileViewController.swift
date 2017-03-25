13f083adf9dd45aa8f8a1726c6cea42cad64a835
Supriya Malgaonkar <supriya.malgaonkar@wohlig.com>
2017-03-25 17:54:18 +0530

    My life text should be Explore

--- TraveLibro/ProfileViewController.swift
+++ TraveLibro/ProfileViewController.swift
@@ -170,9 +170,9 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate,UICollec
         customView = UIView(frame:(CGRect(x: 0, y: self.view.frame.size.height - 75, width: self.view.frame.width, height: 75)))
         self.orangeTab = OrangeButton(frame: CGRect(x: 5, y: self.view.frame.size.height - 125, width: self.view.frame.size.width - 10, height: 50))
         orangeTab.orangeButtonTitle.titleLabel?.font = UIFont(name: "Avenir-Heavy", size: 16)
-        orangeTab.orangeButtonTitle.setTitle("My Life", for: UIControlState())
+        orangeTab.orangeButtonTitle.setTitle("Explore My Life", for: UIControlState())
         let fontAwesomeLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: orangeTab.frame.size.height))
-        fontAwesomeLabel.center = CGPoint(x: 90, y: orangeTab.orangeButtonTitle.titleLabel!.frame.size.height/2 + 10)
+        fontAwesomeLabel.center = CGPoint(x: 150, y: orangeTab.orangeButtonTitle.titleLabel!.frame.size.height/2 + 10)
         fontAwesomeLabel.font = FontAwesomeFont
         fontAwesomeLabel.text = String(format: "%C", faicon["angle_up"]!)
         fontAwesomeLabel.textColor = UIColor.white
